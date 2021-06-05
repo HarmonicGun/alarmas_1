4079fdeeb79369355ba0d58c86feeffa9afb1d58
Jose Martin Aguilar <hummer2.0@hotmail.com>
2021-06-05 17:48:04 -0500

    Add files via upload

---
+++ src/alarmas_1/timer.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alarmas_1;
+
+import java.util.Observable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+ 
+public class timer extends Observable implements Runnable {
+ 
+    private String nombre;
+ 
+    public timer(String nombre) {
+        this.nombre = nombre;
+    }
+ 
+    public String getNombre() {
+        return nombre;
+    }
+ 
+    @Override
+    public void run() {
+ 
+        int porcentaje = 0;
+        int numAleatorio;
+        try {
+            while (porcentaje < 100) {
+                numAleatorio = generaNumeroAleatorio(1, 15);
+                System.out.println("Caballo " + nombre + " ha aumentado en " + numAleatorio);
+                porcentaje += numAleatorio;
+ 
+                this.setChanged();
+                this.notifyObservers(porcentaje);
+                this.clearChanged();
+ 
+                Thread.sleep(1000);
+ 
+            }
+        } catch (InterruptedException ex) {
+            System.out.println("Hilo interrumpido");
+        }
+ 
+    }
+ 
+    public static int generaNumeroAleatorio(int minimo, int maximo) {
+        int num = (int) Math.floor(Math.random() * (maximo - minimo + 1) + (minimo));
+        return num;
+    }
+ 
+}
